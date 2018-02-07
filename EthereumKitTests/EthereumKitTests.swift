021c67ad457b8a17b3e3c12b4bcf9dc6b5803223
yuzushioh <yuzushioh@gmail.com>
2018-02-08 01:37:48 +0900

    delete copy right from the header

--- EthereumKitTests/EthereumKitTests.swift
+++ EthereumKitTests/EthereumKitTests.swift
@@ -3,7 +3,7 @@
 //  EthereumKitTests
 //
 //  Created by yuzushioh on 2018/01/01.
-//  Copyright © 2018 yuzushioh. All rights reserved.
+//  Copyright © 2018 yuzushioh.
 //
 
 import XCTest
@@ -11,28 +11,4 @@ import XCTest
 
 class EthereumKitTests: XCTestCase {
     
-    private var mnemonicManager: MnemonicManager!
-    
-    override func setUp() {
-        super.setUp()
-        mnemonicManager = MnemonicManager(wordList: .english)
-    }
-    
-    func testMnemonicManagerCreateMnemonicSentence() {
-        let resultA = check(
-            mnemonic: "abandon amount liar amount expire adjust cage candy arch gather drum buyer",
-            isEqualToOneCreatedFromEntropy: "000102030405060708090a0b0c0d0e0f"
-        )
-        XCTAssertTrue(resultA)
-        
-        let resultB = check(
-            mnemonic: "swallow abandon ability search abandon abandon abandon able absurd abandon action bench",
-            isEqualToOneCreatedFromEntropy: "DBK3hoIaE1Zdmx/kq/Yu2v1RJ8n3aLAP"
-        )
-        XCTAssertTrue(resultB)
-    }
-    
-    func check(mnemonic: String, isEqualToOneCreatedFromEntropy entropy: String) -> Bool {
-        return mnemonicManager.createMnemonic(fromEntropyString: entropy) == mnemonic
-    }
 }
