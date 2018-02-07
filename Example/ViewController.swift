4ddb6ec14852c37d4468d6abe0e328c030e2b7d7
yuzushioh <yuzushioh@gmail.com>
2018-02-07 12:02:58 +0900

    update read me and sample code

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -23,21 +23,27 @@ class ViewController: UIViewController {
         print(seed)
         // 3779b041fab425e9c0fd55846b2a03e9a388fb12784067bd8ebdb464c2574a05bcc7a8eb54d7b2a2c8420ff60f630722ea5132d28605dbc996c8ca7d7a8311c0
         
-        // By calling initialize, you can now access to your wallet.
         let wallet = Wallet(seed: seed, network: .main)
         
-        // Master private key and master public key generated from the seed.
-        // tprv8ZgxMBicQKsPdM3GJUGqaS67XFjHNqUC8upXBhNb7UXqyKdLCj6HnTfqrjoEo6x89neRY2DzmKXhjWbAkxYvnb1U7vf4cF4qDicyb7Y2mNa
-        // tpubD6NzVbkrYhZ4Wp54C7wRyqkE6HFDYAf6iDRJUDQtXkLEoot6q7usxxHi2tGW48TfY783vGoZ3ufE5XH9YP86c7X6G3CjMh8Dua1ZTTWyjSa
+        // BIP44 key derivation
+        // m/44'
+        let purpose = wallet.privateKey.derived(at: 44, hardens: true)
         
-        let bitcoinPrivateKey = wallet.privateKey.derived(at: 44, hardens: true)
-        let purposePrivateKey = bitcoinPrivateKey.derived(at: 1, hardens: true)
-        let externalPrivateKey = purposePrivateKey.derived(at: 0, hardens: true)
-        let firstPrivateKey = externalPrivateKey.derived(at: 0)
-        let privateKey = firstPrivateKey.derived(at: 0)
+        // m/44'/60'
+        let coinType = purpose.derived(at: 60, hardens: true)
         
-        // tprv8hJrzKEmbFfBx44tsRe1wHh25i5QGztsawJGmxeqryPwdXdKrgxMgJUWn35dY2nrYmomRWWL7Y9wJrA6EvKJ27BfQTX1tWzZVxAXrR2pLLn
-        // tpubDDzu8jH1jdLrqX6gm5JcLhM8ejbLSL5nAEu44Uh9HFCLU1t6V5mwro6NxAXCfR2jUJ9vkYkUazKXQSU7WAaA9cbEkxdWmbLxHQnWqLyQ6uR
-        print(privateKey.publicKey.address)
+        // m/44'/60'/0'
+        let account = coinType.derived(at: 0, hardens: true)
+        
+        // m/44'/60'/0'/0
+        let change = account.derived(at: 0)
+        
+        // m/44'/60'/0'/0
+        let firstPrivateKey = change.derived(at: 0)
+        print(firstPrivateKey.publicKey.address, firstPrivateKey.raw.toHexString())
+        
+        // PrivateKey: ee08ee9b92c8d4cc48f3690c55f70a9974e325a7c228b98ec58dcff8e6d14d66
+        // Address: 0x479c04427c15e84ffd2cbdee136c5a979a08a749
+        // You can check at MyEtherWallet if the private key actually generates the same address.
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # EthereumKit
+
+EthereumKit is a Swift framework that enables you to create Ethereum wallet and use it in your app. 
+Architectures are highly inspired by [WalletKit](https://github.com/yuzushioh/WalletKit) and [BitcoinKit](https://github.com/kishikawakatsumi/BitcoinKit)
+
+🚨 __EthereumKit is currently under development. not ready for the production use__ 🚨
+
+## Features
+- Mnemonic recovery phrease in [BIP39](https://github.com/bitcoin/bips/blob/master/bip-0039.mediawiki)
+- [BIP32](https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki)/[BIP44](https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki) HD wallet
+
+## How to Use
+
+- Generate seed and convert it to mnemonic sentence.
+
+```swift
+let entropy = "000102030405060708090a0b0c0d0e0f"
+let mnemonic = Mnemonic.create(entropy: entropy)
+print(mnemonic)
+// abandon amount liar amount expire adjust cage candy arch gather drum buyer
+        
+let seed = Mnemonic.createSeed(mnemonic: mnemonic)
+print(seed)
+```
+
+- Create your wallet.
+
+```swift
+// It generates master key pair from the seed provided.
+let wallet = Wallet(seed: seed, network: .main)
+```
+
+- Derive private keys at any index from its master private key.
+
+```swift
+// BIP44 key derivation
+// m/44'
+let purpose = wallet.privateKey.derived(at: 44, hardens: true)
+
+// m/44'/60'
+let coinType = purpose.derived(at: 60, hardens: true)
+
+// m/44'/60'/0'
+let account = coinType.derived(at: 0, hardens: true)
+
+// m/44'/60'/0'/0
+let change = account.derived(at: 0)
+
+// m/44'/60'/0'/0
+let firstPrivateKey = change.derived(at: 0)
+print(firstPrivateKey.publicKey.address)
+```
+
+## License
+EthereumKit is released under the [MIT License](LICENSE.md).
