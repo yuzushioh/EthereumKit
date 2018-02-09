dcd3071e705cb60757981cd98e71a7c7cd2f9481
yuzushioh <yuzushioh@gmail.com>
2018-02-10 03:20:48 +0900

    fix generate address bug

--- EthereumKit/PublicKey.swift
+++ EthereumKit/PublicKey.swift
@@ -17,18 +17,22 @@ public struct PublicKey {
     public let index: UInt32
     public let network: Network
     
+    private let privateKey: PrivateKey
+    
     init(privateKey: PrivateKey, chainCode: Data, network: Network, depth: UInt8, fingerprint: UInt32, index: UInt32) {
-        let publicKey = ECDSA.secp256k1.generatePublicKey(with: privateKey.raw, isCompressed: true)
-        self.raw = Data(hex: "0x") + publicKey
+        let compressed = ECDSA.secp256k1.generatePublicKey(with: privateKey.raw, isCompressed: true)
+        self.raw = Data(hex: "0x") + compressed
         self.chainCode = chainCode
         self.depth = depth
         self.fingerprint = fingerprint
         self.index = index
         self.network = network
+        self.privateKey = privateKey
     }
     
     public var address: String {
-        let publicKeyHash = raw.sha3(.keccak256)
-        return "0x" + publicKeyHash.suffix(20).toHexString()
+        let uncompressed = ECDSA.secp256k1.generatePublicKey(with: privateKey.raw, isCompressed: false)
+        let hash = uncompressed.dropFirst().sha3(.keccak256)
+        return "0x" + hash.suffix(20).toHexString()
     }
 }
