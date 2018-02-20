2c30bae4068aea814651db2cbb9488cc6037779d
yuzushioh <yuzushioh@gmail.com>
2018-02-21 00:37:09 +0900

    add sign with private key implementation

--- EthereumKit/Helper/Crypto.swift
+++ EthereumKit/Helper/Crypto.swift
@@ -7,6 +7,7 @@
 //
 
 import EthereumKit.Private
+import secp256k1
 
 final class Crypto {
     static func HMACSHA512(key: Data, data: Data) -> Data {
@@ -23,6 +24,33 @@ final class Crypto {
     
     static func generatePublicKey(data: Data, compressed: Bool) -> Data {
         return Secp256k1.generatePublicKey(withPrivateKey: data, compression: compressed)
+    }
+    
+    static func sign(_ data: Data, privateKey: Data) -> Data {
+        let context = secp256k1_context_create(UInt32(SECP256K1_CONTEXT_SIGN))!
+        defer { secp256k1_context_destroy(context) }
+        
+        let signature = UnsafeMutablePointer<secp256k1_ecdsa_signature>.allocate(capacity: 1)
+        defer { signature.deallocate(capacity: 1) }
+        let status = data.withUnsafeBytes { (ptr: UnsafePointer<UInt8>) in
+            privateKey.withUnsafeBytes { secp256k1_ecdsa_sign(context, signature, ptr, $0, nil, nil) }
+        }
+        
+        guard status == 1 else { fatalError()}
+        
+        let normalizedsig = UnsafeMutablePointer<secp256k1_ecdsa_signature>.allocate(capacity: 1)
+        defer { normalizedsig.deallocate(capacity: 1) }
+        
+        secp256k1_ecdsa_signature_normalize(context, normalizedsig, signature)
+        
+        var length: size_t = 128
+        var der = Data(count: length)
+        guard der.withUnsafeMutableBytes({ return secp256k1_ecdsa_signature_serialize_der(context, $0, &length, normalizedsig) }) == 1 else {
+            fatalError()
+        }
+        
+        der.count = length
+        return der
     }
 }
 
