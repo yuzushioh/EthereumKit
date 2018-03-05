295f5632e643d8040c5c0335c587965b2a432560
yuzushioh <yuzushioh@gmail.com>
2018-03-06 01:08:48 +0900

    fix ecdsa signing

--- EthereumKit/Helper/Crypto.swift
+++ EthereumKit/Helper/Crypto.swift
@@ -27,29 +27,24 @@ final class Crypto {
     }
     
     static func sign(_ data: Data, privateKey: Data) -> Data {
-        let context = secp256k1_context_create(UInt32(SECP256K1_CONTEXT_SIGN))!
+        let context = secp256k1_context_create(UInt32(SECP256K1_CONTEXT_SIGN | SECP256K1_CONTEXT_VERIFY))!
         defer { secp256k1_context_destroy(context) }
         
         let signature = UnsafeMutablePointer<secp256k1_ecdsa_signature>.allocate(capacity: 1)
         defer { signature.deallocate(capacity: 1) }
+        
         let status = data.withUnsafeBytes { (ptr: UnsafePointer<UInt8>) in
             privateKey.withUnsafeBytes { secp256k1_ecdsa_sign(context, signature, ptr, $0, nil, nil) }
         }
         
-        guard status == 1 else { fatalError()}
-        
-        let normalizedsig = UnsafeMutablePointer<secp256k1_ecdsa_signature>.allocate(capacity: 1)
-        defer { normalizedsig.deallocate(capacity: 1) }
-        
-        secp256k1_ecdsa_signature_normalize(context, normalizedsig, signature)
-        
-        var length: size_t = 128
-        var der = Data(count: length)
-        guard der.withUnsafeMutableBytes({ return secp256k1_ecdsa_signature_serialize_der(context, $0, &length, normalizedsig) }) == 1 else {
+        guard status == 1 else {
             fatalError()
         }
         
-        der.count = length
-        return der
+        var output = Data(count: 65)
+        guard output.withUnsafeMutableBytes({ secp256k1_ecdsa_signature_serialize_compact(context, $0, signature) })  == 1 else {
+            fatalError()
+        }
+        return output
     }
 }
