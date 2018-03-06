5fa8d5bd7663fb8c21687ccde7d3de6077f8d615
yuzushioh <yuzushioh@gmail.com>
2018-03-06 17:39:24 +0900

    delete hex extension in Data

--- EthereumKit/Helper/EIP55.swift
+++ EthereumKit/Helper/EIP55.swift
@@ -13,8 +13,8 @@ import CryptoSwift
 
 struct EIP55 {
     static func encode(_ data: Data) -> String {
-        let address = data.hex
-        let hash = address.data(using: .ascii)!.sha3(.keccak256).hex
+        let address = data.toHexString()
+        let hash = address.data(using: .ascii)!.sha3(.keccak256).toHexString()
         
         return zip(address, hash)
             .map { a, h -> String in
