edc643d61774c53c9defc95eff77929ac0a39120
Daisuke Sato <dsk@Daisukes-MacBook-Pro.local>
2018-03-12 17:29:49 +0900

    Add eth_call method

--- EthereumKit/Networking/Geth.swift
+++ EthereumKit/Networking/Geth.swift
@@ -28,6 +28,10 @@ public final class Geth {
         etherClient.send(JSONRPC.SendRawTransaction(rawTransaction: rawTransaction), handler: handler)
     }
     
+    public func call(from: String? = nil, to: String, gas: Int? = nil, gasPrice: Int? = nil, value: Int? = nil, data: String? = nil, blockParameter: BlockParameter = .latest, handler: @escaping (Result<String, GethError>) -> Void) {
+        etherClient.send(JSONRPC.Call(from: from != nil ? Address(string: from!) : nil, to: Address(string: to), gas: gas, gasPrice: gasPrice, value: value, data: data, blockParameter: blockParameter), handler: handler)
+    }
+    
     public func getAccount(address: String, blockParameter: BlockParameter = .latest, handler: @escaping (Result<Account, GethError>) -> Void) {
         getBalance(of: address) { result in
             switch result {
--- EthereumKit/Networking/Request/JSONRPC.swift
+++ EthereumKit/Networking/Request/JSONRPC.swift
@@ -80,4 +80,45 @@ public final class JSONRPC {
             return Response(id: transactionID)
         }
     }
+    
+    public struct Call: JSONRPCKit.Request {
+        public typealias Response = String
+        
+        public let from: Address?
+        public let to: Address
+        public let gas: Int?
+        public let gasPrice: Int?
+        public let value: Int?
+        public let data: String?
+        public let blockParameter: BlockParameter
+        
+        public var method: String {
+            return "eth_call"
+        }
+        
+        public var parameters: Any? {
+            // NOTE: This is to remove key/value if the value is nil.
+            let txParams: [String: Any] = [
+                "from": from?.string as Any?,
+                "to": to.string,
+                "gas": gas,
+                "gasPrice": gasPrice,
+                "value": value,
+                "data": data
+                ].reduce([String: Any]()){ (d, e) in
+                    guard let value = e.value else { return d }
+                    var d = d
+                    d[e.key] = value
+                    return d
+            }
+            return [txParams, blockParameter.rawValue]
+        }
+        
+        public func response(from resultObject: Any) throws -> Response {
+            guard let response = resultObject as? Response else {
+                throw JSONRPCError.unexpectedTypeObject(resultObject)
+            }
+            return response
+        }
+    }
 }
