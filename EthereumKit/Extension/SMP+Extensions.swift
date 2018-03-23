5a87539f550d8a56fc08cd8962189b259c0520a1
yuzushioh <yuzushioh@gmail.com>
2018-03-24 01:52:39 +0900

    fix space

--- EthereumKit/Extension/SMP+Extensions.swift
+++ EthereumKit/Extension/SMP+Extensions.swift
@@ -1,21 +1,22 @@
 // https://github.com/mkrd/Swift-Big-Integer/pull/24
 // TODO: use init(_:, radix:) in SMP but need to fix bugs
 extension BInt {
-  internal init?(str: String, radix: Int) {
-    self.init(0)
-    let bint16 = BInt(16)
-    
-    var exp = BInt(1)
-    
-    for c in str.reversed() {
-      let int = Int(String(c), radix: radix)
-      if int != nil {
-        let value = BInt(int!)
-        self = self + (value * exp)
-        exp = exp * bint16
-      } else {
-        return nil
-      }
+    internal init?(str: String, radix: Int) {
+        self.init(0)
+        let bint16 = BInt(16)
+        
+        var exp = BInt(1)
+        
+        for c in str.reversed() {
+            let int = Int(String(c), radix: radix)
+            if int != nil {
+                let value = BInt(int!)
+                self = self + (value * exp)
+                exp = exp * bint16
+            } else {
+                return nil
+            }
+        }
     }
-  }
 }
+
