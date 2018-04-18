ba9993825e4b5d15dc3140a54428c76270db0fd9
yuzushioh <yuzushioh@gmail.com>
2018-04-18 17:10:28 +0900

    add jsonrpcError to APIClientError

--- EthereumKit/Networking/Client/APIClientError.swift
+++ EthereumKit/Networking/Client/APIClientError.swift
@@ -2,12 +2,16 @@ import APIKit
 
 public enum APIClientError: Error {
     case connectionError(Error)
+    case jsonrpcError(JSONRPCError)
     case systemError(Error)
     
     init(_ error: Error) {
         switch error {
         case SessionTaskError.connectionError(let error):
             self = .connectionError(error)
+            
+        case let error as JSONRPCError:
+            self = .jsonrpcError(error)
             
         default:
             self = .systemError(error)
