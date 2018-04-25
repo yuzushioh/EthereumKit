e041773d7324eae2e98647c3e690c750e9dd961c
yuzushioh <yuzushioh@gmail.com>
2018-04-26 01:17:37 +0900

    use String type for address parameters in JSONRPC class

--- EthereumKit/Networking/Geth.swift
+++ EthereumKit/Networking/Geth.swift
@@ -17,11 +17,11 @@ public final class Geth {
     }
     
     public func getBalance(of address: String, blockParameter: BlockParameter = .latest, handler: @escaping (Result<Balance, EthereumKitError>) -> Void) {
-        etherClient.send(JSONRPC.GetBalance(address: Address(string: address), blockParameter: blockParameter), handler: handler)
+        etherClient.send(JSONRPC.GetBalance(address: address, blockParameter: blockParameter), handler: handler)
     }
     
     public func getTransactionCount(of address: String, blockParameter: BlockParameter = .latest, handler: @escaping (Result<Int, EthereumKitError>) -> Void) {
-        etherClient.send(JSONRPC.GetTransactionCount(address: Address(string: address), blockParameter: blockParameter), handler: handler)
+        etherClient.send(JSONRPC.GetTransactionCount(address: address, blockParameter: blockParameter), handler: handler)
     }
     
     public func sendRawTransaction(rawTransaction: String, handler: @escaping (Result<SentTransaction, EthereumKitError>) -> Void) {
@@ -29,11 +29,11 @@ public final class Geth {
     }
     
     public func call(from: String? = nil, to: String, gasLimit: Gas.GasLimit? = nil, gasPrice: Gas.GasPrice? = nil, value: Int? = nil, data: String? = nil, blockParameter: BlockParameter = .latest, handler: @escaping (Result<String, EthereumKitError>) -> Void) {
-        etherClient.send(JSONRPC.Call(from: from.map(Address.init), to: Address(string: to), gas: gasLimit.map { $0.value }, gasPrice: gasPrice.map { $0.value }, value: value, data: data, blockParameter: blockParameter), handler: handler)
+        etherClient.send(JSONRPC.Call(from: from, to: to, gas: gasLimit.map { $0.value }, gasPrice: gasPrice.map { $0.value }, value: value, data: data, blockParameter: blockParameter), handler: handler)
     }
     
     public func getEstimateGas(from: String? = nil, to: String, gasLimit: Gas.GasLimit? = nil, gasPrice: Gas.GasPrice? = nil, value: Int? = nil, data: String? = nil, handler: @escaping (Result<Wei, EthereumKitError>) -> Void) {
-        etherClient.send(JSONRPC.GetEstimatGas(from: from.map(Address.init), to: Address(string: to), gas: gasLimit.map { $0.value }, gasPrice: gasPrice.map { $0.value }, value: value, data: data), handler: handler)
+        etherClient.send(JSONRPC.GetEstimatGas(from: from, to: to, gas: gasLimit.map { $0.value }, gasPrice: gasPrice.map { $0.value }, value: value, data: data), handler: handler)
     }
     
     public func getBlockNumber(handler: @escaping (Result<Int, EthereumKitError>) -> Void) {
@@ -42,7 +42,7 @@ public final class Geth {
     
     // MARK: - Etherscan APIs
     
-    public func getTransactions(address: String, handler: @escaping (Result<Transactions, EthereumKitError>) -> Void) {
-        etherscanClient.send(Etherscan.GetTransactions(address: Address(string: address)), handler: handler)
+    public func getTransactions(address: String, sort: Etherscan.GetTransactions.Sort = .des, startBlock: Int64 = 0, endBlock: Int64 = 99999999, handler: @escaping (Result<Transactions, EthereumKitError>) -> Void) {
+        etherscanClient.send(Etherscan.GetTransactions(address: address, sort: sort, startBlock: startBlock, endBlock: endBlock), handler: handler)
     }
 }
--- EthereumKit/Networking/Request/Etherscan.swift
+++ EthereumKit/Networking/Request/Etherscan.swift
@@ -8,12 +8,12 @@ public final class Etherscan {
             case asc, des
         }
         
-        public let address: Address
+        public let address: String
         public let sort: Sort
         public let startBlock: Int64
         public let endBlock: Int64
         
-        public init(address: Address, sort: Sort = .asc, startBlock: Int64 = 0, endBlock: Int64 = 99999999) {
+        public init(address: String, sort: Sort, startBlock: Int64, endBlock: Int64) {
             self.address = address
             self.sort = sort
             self.startBlock = startBlock
@@ -28,7 +28,7 @@ public final class Etherscan {
             return [
                 "module": "account",
                 "action": "txlist",
-                "address": address.string,
+                "address": address,
                 "startblock": NSNumber(value: startBlock),
                 "endblock": NSNumber(value: endBlock),
                 "sort": sort.rawValue
