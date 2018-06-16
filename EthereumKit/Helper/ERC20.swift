dd21f6501e81210ef425a7f562d369770dc9ed31
yuzushioh <yuzushioh@gmail.com>
2018-06-16 12:35:50 +0900

    implement erc20 token transfer logic

--- EthereumKit/Helper/ERC20.swift
+++ EthereumKit/Helper/ERC20.swift
@@ -1,5 +1,19 @@
+import CryptoSwift
+
 /// ERC20 contains necessary method to support ERC20 tokens
 public struct ERC20 {
+    
+    /// Transfer method signiture
+    /// function transfer(address _to, uint256 _value) returns (bool success)
+    private static var transferSigniture: Data {
+        let method = "transfer(address,uint256)"
+        return method.data(using: .ascii)!.sha3(.keccak256)[0...3]
+    }
+    
+    /// Length of 256 bits
+    private static var lengthOf256bits: Int {
+        return 256 / 4
+    }
     
     /// Generate transaction data for ERC20 token
     ///
@@ -7,10 +21,61 @@ public struct ERC20 {
     ///    - address: which address to send
     ///    - amount: amount to send
     /// - Returns: transaction data
-    public static func generateData(with address: String, amount: Decimal) -> Data {
-        let transferSignature = Data(bytes: [0xa9, 0x05, 0x9c, 0xbb]).toHexString()
-        let address = address.stripHexPrefix()
-        let amount = (amount * 1e18).toHexString().withLeadingZero(until: 64)
-        return Data(hex: transferSignature + "000000000000000000000000" + address + amount)
+    public static func generateData(with address: String, amount: String, decimal: Int) throws -> Data {
+        let method = transferSigniture.toHexString()
+        let address = pad(string: address.stripHexPrefix())
+        
+        let poweredAmount = try power(amount: amount, decimal: decimal)
+        let amount = pad(string: poweredAmount.serialize().toHexString())
+        
+        return Data(hex: method + address + amount)
+    }
+    
+    /// Power the amount by the decimal
+    ///
+    /// - Parameter:
+    ///    - amount: amount in string format
+    ///    - decimal: specified decimal of this erc20 token
+    /// - Returns: BigInt value powered by (10 * decimal)
+    private static func power(amount: String, decimal: Int) throws -> BInt {
+        let components = amount.split(separator: ".")
+        
+        // components.count must be 1 or 2. this method accepts only integer or decimal value
+        // like 1, 10, 100 or 1.15, 10.7777, 19.9999
+        guard components.count == 1 || components.count == 2 else {
+            throw EthereumKitError.contractError(.containsInvalidCharactor(amount))
+        }
+        
+        guard let integer = BInt(String(components[0]), radix: 10) else {
+            throw EthereumKitError.contractError(.containsInvalidCharactor(amount))
+        }
+        
+        let poweredInteger = integer * (BInt(10) ** decimal)
+        
+        if components.count == 2 {
+            let count = components[1].count
+            
+            guard count <= decimal else {
+                throw EthereumKitError.contractError(.invalidDecimalValue(amount))
+            }
+            
+            guard let digit = BInt(String(components[1]), radix: 10) else {
+                throw EthereumKitError.contractError(.containsInvalidCharactor(amount))
+            }
+            
+            let poweredDigit = digit * (BInt(10) ** (decimal - count))
+            return poweredInteger + poweredDigit
+        } else {
+            return poweredInteger
+        }
+    }
+    
+    /// Pad left spaces out of 256bits with 0
+    private static func pad(string: String) -> String {
+        var string = string
+        while string.count != lengthOf256bits {
+            string = "0" + string
+        }
+        return string
     }
 }
