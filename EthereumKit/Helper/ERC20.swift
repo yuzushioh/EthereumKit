8e3dfeee83aaf257c415c2b8870f24c2270a8a2a
yuzushioh <yuzushioh@gmail.com>
2018-06-16 13:00:34 +0900

    modified ERC20 class interface

--- EthereumKit/Helper/ERC20.swift
+++ EthereumKit/Helper/ERC20.swift
@@ -3,29 +3,50 @@ import CryptoSwift
 /// ERC20 contains necessary method to support ERC20 tokens
 public struct ERC20 {
     
+    /// Represents a contract address of this erc20 token
+    public let contractAddress: String
+    
+    /// Represents a decimal specified in a contract / erc20 token
+    public let decimal: Int
+    
+    /// Represents a symbol of this erc20 token
+    public let symbol: String
+    
+    /// Initializer
+    ///
+    /// - Parameters:
+    ///   - contractAddress: contract address of this erc20 token
+    ///   - decimal: decimal specified in a contract
+    ///   - symbol: symbol of this erc20 token
+    public init(contractAddress: String, decimal: Int, symbol: String) {
+        self.contractAddress = contractAddress
+        self.decimal = decimal
+        self.symbol = symbol
+    }
+    
     /// Transfer method signiture
     /// function transfer(address _to, uint256 _value) returns (bool success)
-    private static var transferSigniture: Data {
+    private var transferSigniture: Data {
         let method = "transfer(address,uint256)"
         return method.data(using: .ascii)!.sha3(.keccak256)[0...3]
     }
     
     /// Length of 256 bits
-    private static var lengthOf256bits: Int {
+    private var lengthOf256bits: Int {
         return 256 / 4
     }
     
     /// Generate transaction data for ERC20 token
     ///
     /// - Parameter:
-    ///    - address: which address to send
+    ///    - to: address you are transfering to
     ///    - amount: amount to send
     /// - Returns: transaction data
-    public static func generateData(with address: String, amount: String, decimal: Int) throws -> Data {
+    public func generateDataParameter(with to: String, amount: String) throws -> Data {
         let method = transferSigniture.toHexString()
-        let address = pad(string: address.stripHexPrefix())
+        let address = pad(string: to.stripHexPrefix())
         
-        let poweredAmount = try power(amount: amount, decimal: decimal)
+        let poweredAmount = try power(amount: amount)
         let amount = pad(string: poweredAmount.serialize().toHexString())
         
         return Data(hex: method + address + amount)
@@ -35,9 +56,8 @@ public struct ERC20 {
     ///
     /// - Parameter:
     ///    - amount: amount in string format
-    ///    - decimal: specified decimal of this erc20 token
     /// - Returns: BigInt value powered by (10 * decimal)
-    private static func power(amount: String, decimal: Int) throws -> BInt {
+    private func power(amount: String) throws -> BInt {
         let components = amount.split(separator: ".")
         
         // components.count must be 1 or 2. this method accepts only integer or decimal value
@@ -71,7 +91,7 @@ public struct ERC20 {
     }
     
     /// Pad left spaces out of 256bits with 0
-    private static func pad(string: String) -> String {
+    private func pad(string: String) -> String {
         var string = string
         while string.count != lengthOf256bits {
             string = "0" + string
